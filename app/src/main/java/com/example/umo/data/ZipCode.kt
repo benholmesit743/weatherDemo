cfa9e285f35152d968e0026022de69daa5727a05
benholmesit743 <benjamin@pearsports.com>
2023-09-08 00:02:31 -0400

    remove unused resources

--- app/src/main/java/com/example/umo/data/ZipCode.kt
+++ app/src/main/java/com/example/umo/data/ZipCode.kt
@@ -13,50 +13,9 @@ data class ZipCode(
     @ColumnInfo(name = "unit") var unit: Int
     )
 
-sealed class ForecastAdapterData{
-    data class DailyForecast(
-        val currentTemp: Double,
-        val hiTemp: Double,
-        val lowTemp: Double,
-        val precipitation: Double,
-        val timeStamp: String
-    ) : ForecastAdapterData()
-    data class Title(val day: Int) : ForecastAdapterData()
-}
-
 fun Int.toUnits() : String {
     return when(this) {
         0 -> "imperial"
         else -> "metric"
     }
 }
-
-//fun CapitalData.toForecastAdapterData(): ArrayList<ForecastAdapterData> {
-//    val result = ArrayList<ForecastAdapterData>()
-//    var currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-//    for (index in hiTemp.indices) {
-//        result.add(ForecastAdapterData.Title(index + 1))
-//        result.add(ForecastAdapterData.DailyForecast(
-//            currentTemp = if (currentHour < hourlyTemps.size) hourlyTemps[currentHour] else 0.00,
-//            hiTemp = hiTemp[index],
-//            lowTemp = if (index < lowTemp.size) lowTemp[index] else 0.00,
-//            precipitation = if (index < precipitation.size) precipitation[index] else 0.00,
-//            timeStamp = timeStamp
-//        ))
-//        currentHour += 24
-//    }
-//    return result
-//}
-//
-//fun CapitalData.getCurrentTemp(): Double {
-//    val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-//    return if (currentHour < hourlyTemps.size) hourlyTemps[currentHour] else 0.00
-//}
-//
-//fun CapitalData.getHiTemp(): Double {
-//    return hiTemp[0]
-//}
-//
-//fun CapitalData.getLowTemp(): Double {
-//    return lowTemp[0]
-//}
